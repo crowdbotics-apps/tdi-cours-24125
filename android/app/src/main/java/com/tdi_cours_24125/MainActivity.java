fa244c9a072a1ebaaaab12b32eb41b0cc5320105
Crowdbotics <team@crowdbotics.com>
2021-01-28 17:53:34 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tdi_cours_24125/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tdi_cours_24125;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "tdi_cours_24125";
+    }
+}
